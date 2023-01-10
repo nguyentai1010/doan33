8058d48ffe8a36258ca8fced3e982378d853b650
nguyentai1010 <111493850+nguyentai1010@users.noreply.github.com>
2023-01-10 10:11:54 +0700

    doan33

---
+++ script.js
@@ -0,0 +1,14 @@
+function Boxes() {
+  let box1Element = document.getElementById(
+    "box-" + document.getElementById("select-1").value
+  );
+  let box2Element = document.getElementById(
+    "box-" + document.getElementById("select-2").value
+  );
+
+  document.getElementById("new-container").innerHTML = "";
+  var clone1 = box1Element.cloneNode(true);
+  var clone2 = box2Element.cloneNode(true);
+  document.getElementById("new-container").appendChild(clone1);
+  document.getElementById("new-container").appendChild(clone2);
+}
